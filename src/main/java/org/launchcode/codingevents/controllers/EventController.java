babee1dc1143c0427ead8ffa9971524f6e8b5b4c
Nicole Holder <nholder06@gmail.com>
2020-03-04 14:11:22 -0600

    could not figure out how to loop through hashmap to display in the table

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,12 +5,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        return "events/index";
+//    }
+
+    private static Map<String, String> events = new HashMap<>();
     @GetMapping
-    public String displayAllEvents(Model model) {
+    public String displayAllEvents(Model model){
+        events.put("Code till Dawn", "Coding meet-up on Fridays until 1AM");
+        events.put("Code With Pride", "Coding meet-up for LGBTQ community");
+        events.put("Strange Loop", "Coding event that sounds fun!");
+        System.out.println(events.values());
         return "events/index";
     }
+    public static Map<String, String> getEvents() {
+        return events;
+    }
 }
+
+
+
