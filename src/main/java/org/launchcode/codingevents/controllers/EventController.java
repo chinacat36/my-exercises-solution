0429748ca288d07a26cdb12ed9779b393628373c
Nicole Holder <nholder06@gmail.com>
2020-04-01 12:22:44 -0500

    Added category field and links to category names

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,16 +1,18 @@
 package org.launchcode.codingevents.controllers;
 
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
-import org.launchcode.codingevents.models.EventType;
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("events")
@@ -19,20 +21,36 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
     @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", EventData.getAll());
-        return "events/index";
+    public String displayAllEvents(@RequestParam(required = false) Integer categoryId, Model model) {
+        if(categoryId == null) {
+            model.addAttribute("title", "All Events");
+            model.addAttribute("events", eventRepository.findAll());
+        }else{
+            Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+            if(result.isEmpty()){
+                model.addAttribute("title", "Invalid Category Id " + categoryId);
+            }else{
+                EventCategory category = result.get();
+                model.addAttribute("title", "Events in Category " + category.getName());
+                model.addAttribute("events", category.getEvents());
+            }
+        }
+            return "events/index";
+
     }
 
     @GetMapping("create")
     public String displayEventForms(Model model){
         model.addAttribute("title", "Create Event");
         model.addAttribute(new Event());
-        model.addAttribute("types", EventType.values());
+        model.addAttribute("categories", eventCategoryRepository.findAll());
         return "events/create";
     }
+
     @PostMapping("create")
     public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
                                          Errors errors, Model model){
@@ -40,15 +58,14 @@ public class EventController {
             model.addAttribute("title", "Create Event");
             return "events/create";
         }
-        EventData.add(newEvent);
-        model.addAttribute("attendees", EventData.getNumOfAttendees(newEvent));
+        eventRepository.save(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
     public String displayDeleteForm(Model model){
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
@@ -57,24 +74,22 @@ public class EventController {
 
         if(eventIds  != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
         return "redirect:";
     }
 
-    @GetMapping("edit/eventId")
+    @GetMapping("edit/{eventId}")
     public String displayEditForm(Model model, @PathVariable int eventId){
-        model.addAttribute("eventId", EventData.getById(eventId));
+//        Event editEvent = eventRepository.findById(eventId);
+        model.addAttribute("eventId", eventRepository.findById(eventId));
         model.addAttribute("title", "Edit Event");
     return "events/edit";
     }
 
     @PostMapping("edit")
     public String processEditForm(int eventId, String name, String description){
-        Event event=EventData.getById(eventId);
-        event.setName(name);
-        event.setDescription(description);
         return "redirect:";
     }
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,11 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import java.util.Objects;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.*;
 
 @Entity
-public class Event {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class Event extends AbstractEntity{
 
     @NotBlank(message = "Email is required.")
     @Email(message = "Invalid email. Try again.")
@@ -37,25 +28,23 @@ public class Event {
     @AssertTrue(message = "You must register")
     private Boolean registration;
 
-    //@Size(min = 0)
-    private int attendees;
-
-    private EventType type;
+    @ManyToOne
+    @NotNull(message = "Category is required.")
+    private EventCategory eventCategory;
 
     public Event() {}
 
     public Event(String name, String description,
                  String contactEmail, String date,
                  String location, Boolean registration,
-                 int attendees, EventType type) {
+                 EventCategory eventCategory) {
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.date = date;
         this.location = location;
         this.registration = registration;
-        this.attendees = attendees;
-        this.type = type;
+        this.eventCategory = eventCategory;
     }
 
     public String getName() {
@@ -106,42 +95,17 @@ public class Event {
         this.registration = registration;
     }
 
-    public int getAttendees() {
-        return attendees;
+    public EventCategory getEventCategory() {
+        return eventCategory;
     }
 
-    public void setAttendees(int attendees) {
-        this.attendees = attendees;
+    public void setEventCategory(EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
     }
-
-    public EventType getType() {
-        return type;
-    }
-
-    public void setType(EventType type) {
-        this.type = type;
-    }
-
-    public int getId() {
-        return id;
-    }
-
 
     @Override
     public String toString() {
         return name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
